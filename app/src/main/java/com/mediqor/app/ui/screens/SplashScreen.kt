0aed443697e2f1747609aa08aaa8ae6654de157a
Pranabudhathoki <pranabudhathoki@gmail.com>
2025-12-23 11:14:30 +0545

    updated

--- app/src/main/java/com/mediqor/app/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/mediqor/app/ui/screens/SplashScreen.kt
@@ -1,18 +1,28 @@
 package com.mediqor.app.ui.screens
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.mediqor.app.ui.navigation.Routes
+import kotlinx.coroutines.delay
 
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavHostController) {
+
+    LaunchedEffect(Unit) {
+        delay(2000)
+        navController.navigate(Routes.LOGIN) {
+            popUpTo(Routes.SPLASH) { inclusive = true }
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "MediQor")
+        Text("MediQor")
     }
 }
