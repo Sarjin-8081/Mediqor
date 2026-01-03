ec823e871231bf4ad9cc44d192dfeb7c21d412bf
Samarth Shah <shahsamarth366@gmail.com>
2026-01-03 23:40:54 +0545

    Updated HomeScreen

--- app/src/main/java/com/mediqor/app/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/mediqor/app/ui/screens/HomeScreen.kt
@@ -1,6 +1,6 @@
 package com.mediqor.app.ui.screens
 
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -18,7 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
